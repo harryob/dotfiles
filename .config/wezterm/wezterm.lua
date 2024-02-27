719aeb8bff9304031107701a41ea5bdd9bfacc5f
Compiler <actions@github.com>
2024-02-27 10:42:52 +0000

    compiled dotfiles

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,11 +3,32 @@ local wezterm = require("wezterm")
 local colors = wezterm.color.load_scheme(wezterm.home_dir .. "/.theme/nightfox.nvim/extra/nordfox/wezterm.toml")
 
 return {
-	window_decorations = "RESIZE",
-	font = wezterm.font("JetBrains Mono"),
-	initial_cols = 160,
-	initial_rows = 45,
-	colors = colors,
-	use_fancy_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
+  window_decorations = "RESIZE",
+  font = wezterm.font("JetBrains Mono"),
+  initial_cols = 160,
+  initial_rows = 45,
+  colors = colors,
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+
+  keys = {
+    {
+      key = '"',
+      mods = "SUPER|SHIFT",
+      action = wezterm.action.SplitPane({
+        direction = "Down",
+        size = { Percent = 30 },
+      }),
+    },
+    {
+      key = "x",
+      mods = "SUPER",
+      action = wezterm.action.PaneSelect({}),
+    },
+    {
+      key = "d",
+      mods = "SUPER",
+      action = wezterm.action.CloseCurrentPane({ confirm = false }),
+    },
+  },
 }
